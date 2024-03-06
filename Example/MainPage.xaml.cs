876ac3240784336de72887495bdf4f6dcb433777
Guilherme Carvalho <guilherme.santos536@etec.sp.gov.br>
2024-03-06 18:25:52 -0300

    fiz uma alteração

---
+++ Example/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Example
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string name = "";
+        string name2 = "";
+
+        //Método Construtor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void OnMessageClicked(object sender, EventArgs e)
+        {
+            name = NameEntry.Text;
+           MessageLabel.Text = $"Hello, {name}!";
+        }
+
+    }
+}
